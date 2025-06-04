022ef7d09f13b9c2c221e9db0a562497f599fc9f
Ibrarr Khan <ibrarr2000@gmail.com>
2025-06-04 17:58:42 +0100

    Add version numbers to files to prevent caching issues

--- inc/styles-scripts.php
+++ inc/styles-scripts.php
@@ -4,24 +4,28 @@ add_action( 'wp_enqueue_scripts', 'add_custom_scripts' );
 function add_custom_scripts() {
     wp_deregister_script( 'jquery-ui' );
 
-    wp_enqueue_style( 'site', VC_TEMPLATE_URI . mix('/dist/css/app.css'), [], null, 'all' );
+    wp_enqueue_style( 'site', VC_TEMPLATE_URI . mix('/css/app.css'), [], null, 'all' );
 
     if ( is_front_page() ) {
-        wp_enqueue_script( 'homepage', VC_TEMPLATE_URI . mix('/dist/js/homepage.js'), [ 'jquery' ], null, true );
+        wp_enqueue_script( 'homepage', VC_TEMPLATE_URI . mix('/js/homepage.js'), [ 'jquery' ], null, true );
     }
 
-    wp_enqueue_script( 'global', VC_TEMPLATE_URI . mix('/dist/js/global.js'), [ 'jquery' ], null, true );
+    wp_enqueue_script( 'global', VC_TEMPLATE_URI . mix('/js/global.js'), [ 'jquery' ], null, true );
 
-    wp_enqueue_script( 'header', VC_TEMPLATE_URI . mix('/dist/js/header.js'), [ 'jquery' ], null, true );
+    wp_enqueue_script( 'header', VC_TEMPLATE_URI . mix('/js/header.js'), [ 'jquery' ], null, true );
 
-    wp_enqueue_script( 'footer', VC_TEMPLATE_URI . mix('/dist/js/footer.js'), [ 'jquery' ], null, true );
+    wp_enqueue_script( 'footer', VC_TEMPLATE_URI . mix('/js/footer.js'), [ 'jquery' ], null, true );
 }
 
-function mix($path) {
+function mix( string $path ) {
     $manifestPath = VC_TEMPLATE_DIR . '/dist/mix-manifest.json';
-    if (file_exists($manifestPath)) {
-        $manifest = json_decode(file_get_contents($manifestPath), true);
-        return $manifest[$path] ?? $path;
+    if ( file_exists( $manifestPath ) ) {
+        $manifest = json_decode( file_get_contents( $manifestPath ), true );
+        // If the manifest has an entry for $path, prefix it with '/dist'
+        if ( isset( $manifest[ $path ] ) ) {
+            return '/dist' . $manifest[ $path ];
+        }
     }
-    return $path;
+    // Fallback to the un-versioned file under /dist
+    return '/dist' . $path;
 }
