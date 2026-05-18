529d9d5bf710b806e5933fc15ce5f2e0b1a53427
Ibrarr Khan <ibrarr2000@gmail.com>
2026-05-18 12:21:02 +0100

    Meta pixel

--- header.php
+++ header.php
@@ -24,15 +24,23 @@
     <script id="cookieyes" async type="text/javascript" src="https://cdn-cookieyes.com/client_data/8d9aad33234792349a620516/script.js"></script>
     <!-- End cookieyes banner -->
 
-    <!-- Start active campaign tracking -->
+    <!-- Meta Pixel Code -->
     <script>
-        (function(e,t,o,n,p,r,i){e.visitorGlobalObjectAlias=n;e[e.visitorGlobalObjectAlias]=e[e.visitorGlobalObjectAlias]||function(){(e[e.visitorGlobalObjectAlias].q=e[e.visitorGlobalObjectAlias].q||[]).push(arguments)};e[e.visitorGlobalObjectAlias].l=(new Date).getTime();r=t.createElement("script");r.src=o;r.async=true;i=t.getElementsByTagName("script")[0];i.parentNode.insertBefore(r,i)})(window,document,"https://diffuser-cdn.app-us1.com/diffuser/diffuser.js","vgo");
-        vgo('setAccount', '802279391');
-        vgo('setTrackByDefault', true);
-
-        vgo('process');
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1622042995562129');
+        fbq('track', 'PageView');
     </script>
-    <!-- End active campaign tracking -->
+    <noscript><img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=1622042995562129&ev=PageView&noscript=1"
+        /></noscript>
+    <!-- End Meta Pixel Code -->
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
