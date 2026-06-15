bafae39f401bed4c525e09f7f59eb8875f162232
Ibrarr Khan <ibrarr2000@gmail.com>
2026-06-16 00:05:18 +0100

    Update headings on post archives

--- home.php
+++ home.php
@@ -17,7 +17,7 @@ get_header();
 <section class="insights-header">
     <div class="container px-4">
         <div class="breadcrumbs"><?php echo do_shortcode('[wpseo_breadcrumb]'); ?></div>
-        <h1 class="insights-title">News, Insights &amp; What We&rsquo;re Building at Vulkan Creative</h1>
+        <h1 class="insights-title">News, Insights &amp; What We&rsquo;re Building at <span>Vulkan Creative</span>.</h1>
         <p class="insights-standfirst">Sharp takes on brand, web and marketing &mdash; what we&rsquo;re learning, building and watching in the industry.</p>
     </div>
 </section>
